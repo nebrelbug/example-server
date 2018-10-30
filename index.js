25e176cddd34dfc6116817dfac1e4c5b7a4b92a0
nebrelbug <nebrelbug@gmail.com>
2018-10-29 20:46:40 -0600

    Added stuff

--- index.js
+++ index.js
@@ -1,38 +1,13 @@
 let express = require('express');
 let path = require('path');
-let Sqrl = require("squirrelly");
 let app = express();
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', function(req, res){
-    res.sendFile('index.html', {templateResult});
-});
-
-app.get('/basic', function(req, res){
-    let myTemplate = `This is a template
-    My favorite template engine is: {{fav}}
-    My favorite kind of cake is: {{cake}}`;
-    
-    let templateResult = Sqrl.Render(myTemplate, {
-        fav: "Squirrelly", cake: "Chocolate"
-    });
-    
-    console.log(templateResult);
-    
-    res.send({basic: templateResult});
-});
-
-app.get('filters', function(req, res){
-    
-    //let myTemplate = 
-});
-
-
-
-// serve static file
-// app.use(express.static(path.join(__dirname, 'public')));
-
-let server = app.listen(4000, function(){
-    console.log("listening to request on port 4000");
+app.set('view engine', 'squirrelly')
+app.get('/', function (req, res) {
+    res.render('index', {
+        name: 'Title',
+        name: 'Ben'
+    })
+})
+let server = app.listen(8080, function () {
+    console.log("listening to requests on port 8080");
 });
